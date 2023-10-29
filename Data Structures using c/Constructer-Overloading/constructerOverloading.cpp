25db382c7273ff43d64213f43892d3d3a45214f6
VIKAS PATEL <43174442+Patelg5606@users.noreply.github.com>
2023-10-29 18:36:13 +0530

    Operator overloading (#33)
    * add code for constOverloading
    
    * update constructor overloading code
    
    * add code for operator loading

---
+++ Data Structures using c/Constructer-Overloading/constructerOverloading.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std; 
+
+class construct 
+{ 
+
+public: 
+	float area; 
+	
+	// Constructor with no parameters 
+	construct() 
+	{ 
+		area = 0; 
+	} 
+	
+	// Constructor with two parameters 
+	construct(int a, int b) 
+	{ 
+		area = a * b; 
+	} 
+	
+	void disp() 
+	{ 
+		cout<< area<< endl; 
+	} 
+}; 
+
+int main() 
+{ 
+	// Constructor Overloading 
+	// with two different constructors 
+	// of class name 
+	construct o; 
+	construct o2( 10, 20); 
+	
+	o.disp(); 
+	o2.disp(); 
+	return 1; 
+} 
